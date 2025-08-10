339da58f592c1e857b60cb46c6834ffae927c164
Aryakuna14 <aryasharan47@gmail.com>
2025-08-10 22:51:20 +0530

    Initial commit: Add takel.py

---
+++ take1.py
@@ -0,0 +1,18 @@
+fame_per = input("Enter famous person: ")
+age = int(input("Enter  age: "))
+nationality = input("enter their country: ")
+famer_per2 = input("Enter another famous person: ")
+age2 = int(input("Enter their age: "))
+nationality2 = input("Enter their country: ")
+
+
+print(f"{fame_per} is {age} years old and is from {nationality}. they are a great person. and have been a vital part of this world and its development.")
+print(f"{famer_per2} is {age2} years old and is from {nationality2}. they are a great person. and have been a vital part of this world and its development.")
+print("This is a great way to learn about famous people and their contributions to society.")
+print("Thank you for sharing these details about famous individuals!")
+print("since both of these people are doing good things in the world, do you think they will ever meet? I think they will, and it will be a great moment in history.")
+a=input("Do you think they will meet? (yes/no): ")
+if a.lower() == "yes":
+    print("I agree, it would be a great moment in history if they met!")
+else:
+    print("That's okay, not everyone has to meet. Their contributions are still significant!")  
